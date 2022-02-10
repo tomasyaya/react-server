1b3ab2f851ee93094ff54d88bcd2ed40d7ac57c8
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2022-02-10 20:25:39 +0100

    test session

--- config/session.config.js
+++ config/session.config.js
@@ -3,21 +3,24 @@ const MongoStore = require("connect-mongo");
 
 // basic session configuration
 // the NODE_ENV comes default with node, you dont need to add it in the .env
+
 function sessionConfig(app) {
   const { NODE_ENV, MONGODB_URL, SESSION_SECRET } = process.env;
+  const isProduction = NODE_ENV === "production";
+  const sameSite = isProduction ? "none" : "lax";
   app.set("trust proxy", 1);
   app.use(
     session({
       secret: SESSION_SECRET,
       resave: true,
-      saveUninitialized: true,
+      saveUninitialized: false,
       store: MongoStore.create({
         mongoUrl: MONGODB_URL,
       }),
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365,
-        sameSite: "none",
-        secure: NODE_ENV === "production",
+        sameSite,
+        secure: isProduction,
       },
     })
   );
